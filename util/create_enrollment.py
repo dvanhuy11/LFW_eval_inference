68a43b6c252281b5a4c40596277a9754e0600e13
HUY DINH <dvanhuy11@gmail.com>
2024-06-19 17:19:02 +0700

    update create enrollment

--- util/create_enrollment.py
+++ util/create_enrollment.py
@@ -8,9 +8,8 @@ from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 from tqdm import tqdm
 
-def extract_face(filename, required_size=(224, 224)):
+def extract_face(filename,detector,required_size=(224, 224)):
     pixels = pyplot.imread(filename)
-    detector = MTCNN()
     results = detector.detect_faces(pixels)
     x1, y1, width, height = results[0]['box']
     x2, y2 = x1 + width, y1 + height
@@ -20,9 +19,9 @@ def extract_face(filename, required_size=(224, 224)):
     face_array = asarray(image)
     return face_array
 
-def get_embeddings(filenames):
-    faces = [extract_face(f) for f in tqdm(filenames, desc="Extracting Faces")]
-    samples = asarray(faces, 'float32')
+def get_embeddings(filenames,detector):
+    faces = [extract_face(f,detector) for f in tqdm(filenames, desc="Extracting Faces")]
+    samples = asarray(faces, 'float32') 
     samples = preprocess_input(samples, version=2)
     model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
     return model.predict(samples, verbose=0)
@@ -30,12 +29,12 @@ def get_embeddings(filenames):
 dataset_directory = '/home/huydinh_intern/lfw'
 output_file_name = '/home/huydinh_intern/enrollment_data.json'
 enrollment_data = []
-
+detector = MTCNN()
 folder_name = [os.path.join(dataset_directory, person) for person in os.listdir(dataset_directory) if os.path.isdir(os.path.join(dataset_directory, person))]
 for person_folder in tqdm(folder_name, desc="Processing Person Folders"):
     images = [os.path.join(person_folder, img) for img in os.listdir(person_folder) if img.endswith('.jpg')]
     if images:
-        feature = get_embeddings([images[0]])[0].tolist()
+        feature = get_embeddings([images[0]],detector)[0].tolist()
         label = os.path.basename(person_folder)
         enrollment_data.append({'feature': feature, 'label': label})
 
